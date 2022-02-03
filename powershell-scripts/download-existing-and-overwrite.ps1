944b4a0eafe8e7d5e0e7038baa5f023be532b44a
Ihor Panasiuk <ihor.panasiuk@nixsolutions.com>
2022-02-03 18:32:19 +0200

    Update download-existing-and-overwrite.ps1

--- powershell-scripts/download-existing-and-overwrite.ps1
+++ powershell-scripts/download-existing-and-overwrite.ps1
@@ -1,20 +1,23 @@
 $bucketName = $Env:GCS_BUCKET;
 $dir = $Env:APP_DIRECTORY;
-$deployPath= $Env:DEPLOY_PATH;
+$deployPath = $Env:DEPLOY_PATH;
 $path = "${dir}/${deployPath}"
-$dataFromBucketDir="./dataFromBucket/${deployPath}"
+$dataFromBucketDir = "./dataFromBucket/${deployPath}"
 
 Invoke-Expression -Command "mkdir ${dataFromBucketDir}"
 Invoke-Expression  -Command "gsutil -m rsync -r gs://${bucketName} ${dataFromBucketDir}"
 
 $appSubFolders = Get-ChildItem $dataFromBucketDir |
-  Where-Object {$_.PSIsContainer} |
-  Foreach-Object {$_.Name}
+Where-Object { $_.PSIsContainer } |
+Foreach-Object { $_.Name }
 
-foreach ($folder in $appSubFolders){
+foreach ($folder in $appSubFolders) {
     if (Test-Path -Path $folder) {
         "Path exists!"
-    } else {
+    }
+    else {
+        $folderToCopy = "${dataFromBucketDir}/${folder}";
+        Write-Output "Copy ${folderToCopy}"
         Copy-Item "${dataFromBucketDir}/${folder}" -Destination $path
     }
 }
