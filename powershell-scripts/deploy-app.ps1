373fe7998d91d873b0ef556623735cf7a362caa6
Ihor Panasiuk <ihor.panasiuk@nixsolutions.com>
2022-02-03 13:58:24 +0200

    Update deploy-app.ps1

--- powershell-scripts/deploy-app.ps1
+++ powershell-scripts/deploy-app.ps1
@@ -1,4 +1,4 @@
 $dir = $Env:APP_DIRECTORY
 $bucket = $Env:GCS_BUCKET
 
-Invoke-Expression "gsutil -m rsync -R '${dir}' gs://${bucket}"
+Invoke-Expression "gsutil -m -h "Cache-Control:private, max-age=0, no-transform" rsync -R '${dir}' gs://${bucket}"
