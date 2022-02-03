cd6f527217b6563bfbdaaf9c3c3ce3dda8dd24b1
Ihor Panasiuk <ihor.panasiuk@nixsolutions.com>
2022-02-03 18:15:17 +0200

    das

--- powershell-scripts/download-existing-and-overwrite.ps1
+++ powershell-scripts/download-existing-and-overwrite.ps1
@@ -2,4 +2,5 @@ $dir = $Env:APP_DIRECTORY
 $dataFromBucketDir="./dataFromBucket"
 $bucket = $Env:GCS_BUCKET
 
+Invoke-Expression -Command "mkdir ${dataFromBucketDir}"
 Invoke-Expression  -Command "gsutil -m rsync -r -x 'apps/*' gs://${bucket} ${dataFromBucketDir}"
